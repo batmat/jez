02f8f3daf77610cb310edccf4e5fe882e771bb95
Baptiste Mathus <batmat@batmat.net>
2015-12-14 22:03:06 +0100

    Secure and make the user/password variables

--- jenkins-master/init_scripts/create_admin_user.groovy
+++ jenkins-master/init_scripts/create_admin_user.groovy
@@ -12,14 +12,19 @@ assert adminPassword != null : "No ADMIN_PASSWORD env var provided, but required
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 // FIXME : just during debugging/initial dev, remove the password part of that log
-println "XXXXXXXXXXXXXXXXXX Creating the '$adminUserName' admin user with password '$adminPassword'"
+println "Creating the '$adminUserName' admin user with provided password (using env var 'ADMIN_PASSWORD')"
+
+if(adminPassword.equals(System.getenv("DEFAULT_ADMIN_PASSWORD"))) {
+  println("WARNING: You didn't change the default image password, there may be a security risk")
+  println("WARNING: Pass the value using 'docker run -e ADMIN_PASSWORD=theOneYouWant ...'")
+}
 hudsonRealm.createAccount("admin", System.getenv("ADMIN_PASSWORD"))
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new GlobalMatrixAuthorizationStrategy()
 strategy.add(Jenkins.ADMINISTER, "admin")
 
-strategy.add(Jenkins.READ, "Anonymous")
+strategy.add(Jenkins.READ, "anonymous")
 instance.setAuthorizationStrategy(strategy)
 
 instance.save()
